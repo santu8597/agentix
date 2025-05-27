a8952d54e4e0b77cd4a5dc87c5cb899412a21c47
Diya <diyadey310804@gmail.com>
2025-05-27 15:18:44 +0530

    agent_page_ui_updated

--- app/my-agents/page.tsx
+++ app/my-agents/page.tsx
@@ -1,12 +1,22 @@
 import { MyAgentsList } from "@/components/blockchain/my-agents-list"
 import Navbar from "@/components/navbar"
+import { cn } from "@/lib/utils";
+import { GridPattern } from "@/components/magicui/grid-pattern";
+import { InteractiveGridPattern } from "@/components/magicui/interactive-grid-pattern";
+
 
 export default function MyAgentsPage() {
   return (
+    
     <div className="flex flex-col min-h-screen">
+      
       <Navbar />
-
+    <InteractiveGridPattern className={cn(
+          "[mask-image:radial-gradient(750px_circle_at_center,white,transparent)]",
+          "inset-x-auto inset-y-auto  h-[200%] skew-y-6")}/>
+      
       <main className="flex-1 container py-8">
+         
         <div className="flex justify-between items-center mb-8">
           <h2 className="text-3xl font-bold">My Purchased Agents</h2>
         </div>
@@ -23,5 +33,6 @@ export default function MyAgentsPage() {
         </div>
       </footer>
     </div>
+    
   )
 }
